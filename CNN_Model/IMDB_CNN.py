f3bf3f2746139cd15e25a68406caa6c8bb71877b
Richard Soon <topsun888@yahoo.com>
2018-06-26 21:56:54 +0800

    Add CNNRNN and two-layer CNN models

--- CNN_Model/IMDB_CNN.py
+++ CNN_Model/IMDB_CNN.py
@@ -3,11 +3,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 import sys
+import tensorflow as tf
 path = './tools'
 sys.path.append(path)
 
 
 def read_data():
+    '''
+    Read data from local file
+    '''
     train_data = pd.read_csv('./data/IMDB_review_train.csv')
     test_data = pd.read_csv('./data/IMDB_review_test.csv')
     train_texts = list(train_data.text.values)
@@ -16,8 +20,10 @@ def read_data():
     test_labels = list(test_data.sentiment.values)
     return train_texts, test_texts, train_labels, test_labels
 
-if __name__ == '__main__':
-    train_texts, test_texts, train_labels, test_labels = read_data()
+def process_data(train_texts, test_texts):
+    '''
+    Clean the texts and map words into IDs
+    '''
     from text_preprocess import text_clean
     tc = text_clean(train_texts)
     train_processed = tc.proceed()
@@ -32,8 +38,15 @@ if __name__ == '__main__':
     ti = token2idx(train_sent_words, 30000)
     train_sent_idx = ti.proceed()
     test_sent_idx = ti.map_text_idx(test_sent_words, ignore_sent=True)
+    return train_sent_idx, test_sent_idx, ti
+
+
+if __name__ == '__main__':
+    train_texts, test_texts, train_labels, test_labels = read_data()
+    train_sent_idx, test_sent_idx, ti = process_data(train_texts, test_texts)
     n_words = len(ti.get_vocab())
     print('Total words: %d' % n_words)
+    print('Data Processed!')
 
     from sample_generator import generate_samples
     MAX_DOCUMENT_LENGTH = 800
@@ -70,6 +83,78 @@ if __name__ == '__main__':
     test_chunk_num = int(len(test_texts)/trainConfig.batch_size)
     remain_num = len(test_texts) - trainConfig.batch_size*test_chunk_num
     print(remain_num)
+
+    print('Build Graph')
+    from model import CNN_Model
+    graph_cnn = tf.Graph()
+    #Create models for training and testing data
+    with graph_cnn.as_default():
+        initializer = tf.random_uniform_initializer(-0.02, 0.02)
+        with tf.name_scope('train'):
+            train_data = tf.placeholder(tf.int32, [trainConfig.batch_size, MAX_DOCUMENT_LENGTH])
+            train_label = tf.placeholder(tf.int32, [trainConfig.batch_size])
+            train_lengths = tf.placeholder(tf.int32, [trainConfig.batch_size])
+            #Set different models for different buckets
+            with tf.variable_scope("Model", reuse=None, initializer=initializer):
+                train_model = CNN_Model(trainConfig, train_data, train_label, train_lengths)
+                saver=tf.train.Saver()
+        with tf.name_scope('test'):
+            test_data = tf.placeholder(tf.int32, [testConfig.batch_size, None])
+            test_label = tf.placeholder(tf.int32, [testConfig.batch_size])
+            test_lengths = tf.placeholder(tf.int32, [testConfig.batch_size])
+            single_data = tf.placeholder(tf.int32, [singleConfig.batch_size, None])
+            single_label = tf.placeholder(tf.int32, [singleConfig.batch_size])
+            single_lengths = tf.placeholder(tf.int32, [singleConfig.batch_size])
+            #Set different models for different buckets
+            with tf.variable_scope("Model", reuse=True, initializer=initializer):
+                test_model = CNN_Model(testConfig, test_data, test_label, test_lengths, False)
+                single_model = CNN_Model(singleConfig, single_data, single_label, single_lengths, False)
+
+    
+    import time, os
+    epochs = 3
+    #train_chunk_num = 10
+    file = "ckpt_cnn/cnn.ckpt"
+    with tf.Session(graph=graph_cnn) as sess:
+        #Initialize parameters
+        init = tf.global_variables_initializer()
+    
+        if os.path.exists("ckpt_cnn/cnn.ckpt.index"):
+            saver.restore(sess, file)
+        else:
+            sess.run(init)
+        start_time = time.time()
+        for m in range(epochs):
+            for i in range(train_chunk_num):
+                #sess.run(tf.assign(learning_rate, 0.002*((0.98)**m)))
+                x, y, lengths, _ = gs_train.generate_batch(trainConfig.batch_size)
+                feed_dict = {train_data:x, train_label:y, train_lengths:lengths}
+                l, _ = sess.run([train_model.cost, train_model.optimize], feed_dict=feed_dict)
+                if i%100 == 0:
+                    print('Loss:', round(l, 4))
+            end_time = time.time()
+            print('Epoch', m, 'time:{:.2f}'.format(end_time - start_time))
+            start_time = end_time
+        saver.save(sess,'ckpt_cnn/cnn.ckpt')
+        #Calculate Testing Accuracy
+        print('Testing...')
+        count = 0
+        gs_test = generate_samples(test_sent_idx, test_labels, MAX_DOCUMENT_LENGTH)
+        for _ in range(test_chunk_num):
+            #Traverse each data
+            x, y, lengths, _ = gs_test.generate_batch(testConfig.batch_size, False)
+            feed_dict = {test_data:x, test_label:y, test_lengths:lengths}
+            n = sess.run(test_model.correct_num, feed_dict=feed_dict)
+            count += np.sum(n)
+        for _ in range(remain_num):
+            #Traverse each data
+            x, y, lengths, _ = gs_test.generate_batch(1, False)
+            feed_dict = {single_data:x, single_label:y, single_lengths:lengths}
+            n = sess.run(single_model.correct_num, feed_dict=feed_dict)
+            count += np.sum(n)
+        end_time = time.time()
+        print('Testing Time:{:.2f}'.format(end_time - start_time))
+        print(count*1.0/len(test_texts))  
 
     
 
---
+++ CNN_Model/tools/token_idx_map.py
@@ -0,0 +1,142 @@
+from collections import Counter
+from time import time
+from textblob import TextBlob
+class token2idx:
+    '''
+    Build a vacabulary and dictionary of tokens
+    Map each token into an unique ID
+    Note, a text can be viewed as a series of sentences
+    a sentence consist of words
+    '''
+    def __init__(self, texts, vocab_size=20000):
+        '''
+        Args:
+        texts: a list of words or lists or a list of textblob objects
+        vocab_size: length of the vocabulary
+        '''
+        assert isinstance(texts, list) == True
+        self.__is_textblob = isinstance(texts[0], TextBlob)
+        #sentences
+        self.__texts = texts
+        self.__vocab_size = vocab_size
+        self.__word_idx_dict = None
+        self.__idx_word_dict = None
+        self.__vocab = None
+
+    def proceed(self, ignore_sent=True):
+        '''
+        Flatten word lists and build vocabulary
+        Args:
+        ignore_sent: bool, only valid for textblob object
+        '''
+        #Flatten word lists
+        print('Start mapping words to IDs....')
+        start = time()
+        if not self.__is_textblob:
+            words_all = list(self.__flatten__(self.__texts))
+        else:
+            words_all = self.__flatten_textblob__(self.__texts)
+        #Calculate word frequences
+        word_freq_pair = Counter(words_all)
+        #Select the most common ones
+        if len(word_freq_pair) <= self.__vocab_size:
+            #word_common = word_freq_pair
+            self.__vocab_size = len(word_freq_pair)
+        word_common = word_freq_pair.most_common(self.__vocab_size)
+        #Build a vocabulary and dictionaries
+        self.__vocab, _ = list(zip(*word_common))
+        self.__word_idx_dict = {word: (i+1) for i, word in enumerate(self.__vocab)}
+        #Add one more as unknown symbols
+        #Set unknow words as '0'
+        self.__word_idx_dict['*unknown*'] = 0
+        #Build an id-to-word dictionary
+        self.__idx_word_dict = dict(zip(self.__word_idx_dict.values(), 
+                                         self.__word_idx_dict.keys()))
+        #Append unknow to the vocab
+        self.__vocab = list(self.__vocab)
+        self.__vocab.insert(0, '*unknown')
+
+        #Map the texts into series of IDs
+        texts_idx = self.map_text_idx(self.__texts, ignore_sent)
+            
+        end = time()
+        print('Processing Finished! Timing: ', round(end-start, 3))
+        return texts_idx
+
+    def map_text_idx(self, texts, ignore_sent):
+        '''
+        Map text of words into idx, this is for testing data
+        '''
+        texts_idx = []
+        if self.__is_textblob:
+            for blob in texts:
+                if ignore_sent:
+                    word_idx = list(map(self.__word2idx__, blob.words))
+                    texts_idx.append(word_idx)
+                else:
+                    text_idx = []
+                    for sent in blob.sentences:
+                        sent_idx = list(map(self.__word2idx__, sent.words))
+                        text_idx.append(sent_idx)
+                    texts_idx.append(text_idx)
+            return texts_idx
+        #Traverse each text
+        for text in texts:
+            text_idx = []
+            if (len(text) > 0) and isinstance(text[0], str):
+                text_idx = list(map(self.__word2idx__, text))
+            elif (len(text) > 0) and isinstance(text[0], list):
+                #Traverse each sentence
+                for sent in text:
+                    #If a text consists of sentences
+                    if isinstance(sent, list):
+                        sent_idx = list(map(self.__word2idx__, sent))
+                        text_idx.append(sent_idx)
+            texts_idx.append(text_idx)
+        return texts_idx
+    
+    def get_vocab(self):
+        '''
+        Return the vocabulary of training data set
+        '''
+        #vocab = list(self.__vocab)
+        #return vocab.insert(0, 'unknown')
+        return self.__vocab
+
+    def __word2idx__(self, word):
+        '''
+        Map a word into an ID
+        '''
+        try:
+            ID = self.__word_idx_dict[word]
+        #Unknown words as '0'
+        except:
+            ID = 0
+        return ID
+
+    def __idx2word__(self, ID):
+        '''
+        Map an ID into a word
+        '''
+        try:
+            word = self.__idx_word_dict[ID]
+        except:
+            word = '*unknown*'
+        return word
+
+    ##https://www.zhihu.com/question/27010691/answer/248832634
+    def __flatten__(self, l): 
+        '''Flatten all the lists into a big one'''
+        for k in l: 
+            if not isinstance(k, (list, tuple)): 
+                yield k 
+            else: 
+                yield from self.__flatten__(k)
+                
+    def __flatten_textblob__(self, blobs):
+        '''Flatten all the lists into a big one'''
+        words_total = []
+        for blob in blobs:
+            words_total.extend(blob.words)
+        return words_total
+
